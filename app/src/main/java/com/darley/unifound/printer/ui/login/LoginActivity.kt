18fe9fc0b5f0d90f7b7523c9d464aa1239c4dd8c
Darley-Wey <darley.wey@gmail.com>
2022-08-10 00:03:28 +0800

    移除存储权限

--- app/src/main/java/com/darley/unifound/printer/ui/login/LoginActivity.kt
+++ app/src/main/java/com/darley/unifound/printer/ui/login/LoginActivity.kt
@@ -1,12 +1,11 @@
 package com.darley.unifound.printer.ui.login
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.DocumentsContract
 import android.provider.Settings
 import android.text.Editable
 import android.text.TextWatcher
@@ -15,93 +14,24 @@ import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat.requestPermissions
-import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.startActivity
-import androidx.documentfile.provider.DocumentFile
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.darley.unifound.printer.APP
 import com.darley.unifound.printer.R
 import com.darley.unifound.printer.data.model.LoggedInUser
 import com.darley.unifound.printer.databinding.ActivityLoginBinding
-import com.darley.unifound.printer.ui.printer.PrinterActivity
+import com.darley.unifound.printer.ui.printer.UploadActivity
 
 class LoginActivity : AppCompatActivity() {
+
 
     private lateinit var loginViewModel: LoginViewModel
     private lateinit var binding: ActivityLoginBinding
 
 
-    /*@Deprecated("Deprecated in Java")
-    override fun onActivityResult(
-        requestCode: Int, resultCode: Int, resultData: Intent?,
-    ) {
-        super.onActivityResult(requestCode, resultCode, resultData)
-        if (requestCode == 0
-            && resultCode == Activity.RESULT_OK
-        ) {
-            // The result data contains a URI for the document or directory that
-            // the user selected.
-            resultData?.also {
-                contentResolver.takePersistableUriPermission(
-                    it.data!!, Intent.FLAG_GRANT_READ_URI_PERMISSION
-                            or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-                // Perform operations on the document using its URI.
-            }
-        }
-    }*/
-
-
-    //  开启存储权限
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray,
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            0 -> {
-                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "请手动开启存储权限，否则无法使用", Toast.LENGTH_SHORT).show()
-                    val b = shouldShowRequestPermissionRationale(permissions[0])
-                    if (!b) {
-                        showDialogTipUserGoToAppSetting()
-                    } else {
-                        finish()
-                    }
-                } else {
-                    Toast.makeText(this, "存储权限获取成功", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-
-    }
-
-    private fun showDialogTipUserGoToAppSetting() {
-        val dialog = android.app.AlertDialog.Builder(this)
-        dialog.setTitle("存储权限未开启")
-        dialog.setMessage("请在-应用设置-权限-中，允许使用存储权限来保存您的数据")
-        dialog.setPositiveButton("立即开启") { _, _ ->
-            val intent =
-                Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-            intent.data = Uri.parse("package:$packageName")
-            startActivity(intent)
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//      检查存储权限
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 0)
-        }
 
+        // TODO webView
         /*val webView = WebView(this)
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = WebViewClient()
@@ -121,9 +51,6 @@ class LoginActivity : AppCompatActivity() {
         val password = binding.password
         val login = binding.login
         val loading = binding.loading
-
-        //        loginViewModel =
-        //            ViewModelProvider(this, LoginViewModelFactory())[LoginViewModel::class.java]
 
 
         // 监听输入框的变化，实时错误检查
@@ -147,11 +74,10 @@ class LoginActivity : AppCompatActivity() {
                 showLoginFailed(loginResult.error)
             }
             if (loginResult.success != null) {
+                //Complete and destroy login activity once successful
                 updateUiWithUser(loginResult.success)
             }
             setResult(Activity.RESULT_OK)
-            //Complete and destroy login activity once successful
-//            finish()
         })
 
         // 监听登录网络请求
@@ -159,7 +85,6 @@ class LoginActivity : AppCompatActivity() {
             if (it.isSuccess) {
                 it.getOrNull()
                     ?.let { it1 -> loginViewModel.loginResult(it1) }
-//                        ?.let { it2 -> updateUiWithUser(it2) }
             } else {
                 loading.visibility = View.GONE
                 showLoginFailed("登陆失败，请检查输入或网络连接")
@@ -204,13 +129,12 @@ class LoginActivity : AppCompatActivity() {
     private fun updateUiWithUser(model: LoggedInUser) {
         val welcome = getString(R.string.welcome)
         val displayName = model.displayName
-        // TODO : initiate successful logged in experience
         Toast.makeText(
             applicationContext,
             "$welcome $displayName",
             Toast.LENGTH_LONG
         ).show()
-        PrinterActivity.actionStart(this, "login")
+        UploadActivity.actionStart(this, "login")
         finish()
     }
 
